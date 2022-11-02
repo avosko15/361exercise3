9074ac97065165fa899fb2bdfb74a922092232f5
Anna <73409556+avosko15@users.noreply.github.com>
2022-11-02 10:16:12 -0700

    I cant get the unexpected end-of-input error to go away

--- main.rb
+++ main.rb
@@ -1,20 +1,77 @@
-class Student
+class MentalState
+	def auditable?
+		# true if the external service is online, otherwise false
+	end
 
-  MAX_COURSES = 5
+	def audit!
+		# Could fail if external service is offline
+		raise OfflineException.new('External service is offline') unless auditable?
+	end
 
-  def remove_from_schedule(course, schedule, quarter_id)
-    schedule.quarter[quarter_id].course_list.remove(course)
-  end
+	def do_work
+		# Amazing stuff...
+	end
+end
 
-  def add_to_schedule(course, schedule, quarter_id)
-    quarter = schedule.quarter[quarter_id]
-    course_list = quarter.course_list
-    max_courses = course_list.maximum_number_of_courses
+def audit_sanity(bedtime_mental_state)
+	if bedtime_mental_state.audit!.ok?
+		MorningMentalState.new(:ok)
+	else 
+		MorningMentalState.new(:not_ok)
+	end
+end
 
-    if max_courses < MAX_COURSES
-        course_list.add(course)
-    end
-  end
+begin 
+	new_state = audit_sanity(bedtime_mental_state)
+rescue OfflineException => e
+	e.message()
 end
 
 
+
+# Exercise 5 Part 2 (Don't Return Null / Null Object Pattern)
+
+class BedtimeMentalState < MentalState ; end
+
+class MorningMentalState < MentalState ; end
+
+def audit_sanity(bedtime_mental_state)
+	if bedtime_mental_state.audit!.ok?
+		MorningMentalState.new(:ok)
+	else
+		MorningMentalState.new(:not_ok)
+	end
+end
+
+if bedtime_mental_state.auditable?
+	new_state = audit_sanity(bedtime_mental_state)
+	new_state.do_work
+  
+
+# Exercise 5 Part 3 (Wrapping APIs)
+
+require 'candy_service'
+
+class Machine
+	@machine = CandyMachine.new
+
+	def prepare
+		machine.prepare
+	end
+
+	def ready?
+		machine.ready?
+	end
+
+	def make!
+		machine.make!
+	end
+end
+
+machine = Machine.new
+machine.prepare
+if machine.ready?
+	machine.make!
+else
+	puts "sadness"
+end
